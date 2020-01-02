9048a0694da6e1b67beae4495e92fdd8cc69f728
wangdm <wangdm1989@gmail.com>
2020-01-02 20:06:40 +0800

    add dbus tutorial

--- dbus/receiver.c
+++ dbus/receiver.c
@@ -22,6 +22,42 @@ static void dbus_toggled_handle(DBusWatch *watch, void *data) {
     printf("dbus_toggled_handle\n");
 }
 
+static DBusHandlerResult dbus_message_handle1(DBusConnection *connection, DBusMessage *message, void *user_data) {
+    printf("receive message int handler1\n");
+    int type = dbus_message_get_type(message);
+    const char *path = dbus_message_get_path(message);
+    const char *interface = dbus_message_get_interface(message);
+    const char *member = dbus_message_get_member(message);
+    const char *sender = dbus_message_get_sender(message);
+    const char *dest = dbus_message_get_destination(message);
+    printf("type: %s\n", dbus_message_type_to_string(type));
+    printf("path: %s\n", path);
+    printf("interface: %s\n", interface);
+    printf("member: %s\n", member);
+    printf("sender: %s\n", sender);
+    printf("dest: %s\n", dest);
+    printf("\n");
+    return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
+}
+
+static DBusHandlerResult dbus_message_handle2(DBusConnection *connection, DBusMessage *message, void *user_data) {
+    printf("receive message int handler2\n");
+    int type = dbus_message_get_type(message);
+    const char *path = dbus_message_get_path(message);
+    const char *interface = dbus_message_get_interface(message);
+    const char *member = dbus_message_get_member(message);
+    const char *sender = dbus_message_get_sender(message);
+    const char *dest = dbus_message_get_destination(message);
+    printf("type: %s\n", dbus_message_type_to_string(type));
+    printf("path: %s\n", path);
+    printf("interface: %s\n", interface);
+    printf("member: %s\n", member);
+    printf("sender: %s\n", sender);
+    printf("dest: %s\n", dest);
+    printf("\n");
+    return DBUS_HANDLER_RESULT_HANDLED;
+}
+
 int main(int argc, char **argv) {
     int ret;
     DBusError error;
@@ -44,11 +80,16 @@ int main(int argc, char **argv) {
     //dbus_bus_add_match(connection, "type='signal'", &error);
     dbus_bus_add_match(connection, "type='signal',interface='cn.wangdm.dbus'", &error);
 
+    dbus_connection_add_filter(connection, dbus_message_handle2, NULL, NULL);
+    dbus_connection_add_filter(connection, dbus_message_handle1, NULL, NULL);
+
     ret = dbus_connection_set_watch_functions(connection, dbus_add_handle, dbus_remove_handle, dbus_toggled_handle,
                                               NULL, NULL);
     if (TRUE != ret) {
         printf("dbus_connection_set_watch_functions failed\n");
     }
+
+    DBusDispatchStatus status = dbus_connection_read_write_dispatch(connection, 0);
 
     do {
         ret = dbus_connection_read_write(connection, 3000);
@@ -58,13 +99,13 @@ int main(int argc, char **argv) {
         }
         DBusMessage *message = dbus_connection_pop_message(connection);
         if (message) {
-            printf("receive message\n");
+            printf("receive message in loop\n");
             int type = dbus_message_get_type(message);
-            const char* path = dbus_message_get_path(message);
-            const char* interface = dbus_message_get_interface(message);
-            const char* member = dbus_message_get_member(message);
-            const char* sender = dbus_message_get_sender(message);
-            const char* dest = dbus_message_get_destination(message);
+            const char *path = dbus_message_get_path(message);
+            const char *interface = dbus_message_get_interface(message);
+            const char *member = dbus_message_get_member(message);
+            const char *sender = dbus_message_get_sender(message);
+            const char *dest = dbus_message_get_destination(message);
             printf("type: %s\n", dbus_message_type_to_string(type));
             printf("path: %s\n", path);
             printf("interface: %s\n", interface);
--- dbus/sender.c
+++ dbus/sender.c
@@ -8,20 +8,7 @@
 
 #include "dbus/dbus.h"
 
-static dbus_bool_t dbus_add_handle(DBusWatch *watch, void *data){
-    printf("dbus_add_handle\n");
-    return TRUE;
-}
-
-static void dbus_remove_handle(DBusWatch *watch, void *data){
-    printf("dbus_remove_handle\n");
-}
-
-static void dbus_toggled_handle(DBusWatch *watch, void *data){
-    printf("dbus_toggled_handle\n");
-}
-
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
     int ret;
     DBusError error;
     dbus_error_init(&error);
@@ -38,13 +25,6 @@ int main(int argc, char **argv) {
     ret = dbus_bus_request_name(connection, "dbus.sender", DBUS_NAME_FLAG_REPLACE_EXISTING, &error);
     if (TRUE != ret) {
         printf("dbus_bus_request_name failed, name:%s, message:%s\n", error.name, error.message);
-    }
-
-    dbus_bus_add_match(connection, "", &error);
-
-    ret = dbus_connection_set_watch_functions(connection, dbus_add_handle, dbus_remove_handle, dbus_toggled_handle, NULL, NULL);
-    if (TRUE != ret){
-        printf("dbus_connection_set_watch_functions failed\n");
     }
 
     DBusMessage *message = dbus_message_new_signal("/test", "cn.wangdm.dbus", "test");
